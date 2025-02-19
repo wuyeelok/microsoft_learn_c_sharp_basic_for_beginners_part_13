60695581d6722a13d01cd4ed9bd0e59a0a208a83
wuyeelok <wuyeelok@gmail.com>
2025-02-19 11:10:51 +0800

    begin learn part 13

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var greetings = new List<String>{
+    "Hello",
+    "World,",
+    "Part 13!"
+};
+
+var msg = "";
+foreach (var greeting in greetings)
+{
+    msg += " " + greeting;
+}
+
+Console.WriteLine(msg);
